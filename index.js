8b71610bd216857273e5d06e10908f6e066312a5
Lili Fuentes <lile7886@gmail.com>
2020-10-02 12:56:15 -0400

    updated code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
          'Michigan', 'Georgia', 'Vermont'], correctAnswer: 'Georgia'},
          {question: 'Who is the proverbial "King of the Jungle"?', answer: ['Jorge', 'Tiger', 'Lion',
          'Tarzan'], correctAnswer: 'Lion'},
-         {question: 'What does the "19" in COVID-19 stand for?', answer: ['The year', 'Strain store.questionNumber', 
+         {question: 'What does the "19" in COVID-19 stand for?', answer: ['The year', 'Strain', 
          'Amount of people it killed', 'No one knows, really'], correctAnswer: 'The year'},
          {question: 'How many colors are in the rainbow?', answer: ['12', '7', '5', '36'], 
          correctAnswer: '7'},
@@ -28,120 +28,85 @@
  let question = store.questionNumber;
 
 
-//   let q = 0;
-//  let rb1 = 0;
-//  let rb2 = 1;
-//  let rb3 = 2;
-//  let rb4 = 3;
-//  let ans = 0;
-
-//  let userScore = {right : 0, wrong : 0};
-
-  $('main').append('<button id="start" class="begin">Start Quiz</button>');
-
-  $('#start').on('click', function() {
-       $('main').html(getHTML(store));
-       $('#submit').on('click', function() {
-         $('main').html(rightAns(store));
-         $('#next').on('click', function() {
-            question++;
-             $('main').html(getHTML(store));
-            $('#submit').on('click', function() {
-                $('main').html(rightAns(store));
-                $('#next').on('click', function() {
-                    question++;
-                    $('main').html(getHTML(store));
-                    $('#submit').on('click', function() {
-                        $('main').html(rightAns(store));
-                        $('#next').on('click', function() {
-                            question++;
-                            $('main').html(getHTML(store));
-                            $('#submit').on('click', function() {
-                                $('main').html(rightAns(store));
-                                $('#next').on('click', function() {
-                                    question++;
-                                    $('main').html(getHTML(store));
-                                    $('#submit').on('click', function() {
-                                        $('main').html(rightAns(store));
-                                        $('#next').on('click', function() {
-                                            question++;
-                                            $('main').html(getHTML(store));
-                                            $('#submit').on('click', function() {
-                                                $('main').html(rightAns(store));
-                                                $('#next').on('click', function() {
-                                                    question++;
-                                                    $('main').html(getHTML(store));
-                                                });
-                                            });
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-            });
-        });
-    });
-  });
+$('main').append('<button id="start" class="begin">Start Quiz</button>');
+$('#start').on('click', function () {
+    loadQuestion(store);
 });
 
- function getHTML(store) {
-    
-    return `<div class="questions">
-    <h2 id="question">${store.questions[question].question}</h2>
-        <form>
-        <input type="radio" id="ans1" name="quest" value=${store.questions[question].answer[0]}>
-        <label for="male">${store.questions[question].answer[0]}</label><br>
 
-        <input type="radio" id="ans2" name="quest" value=${store.questions[question].answer[1]}>
-        <label for="female">${store.questions[question].answer[1]}</label><br>
-
-        <input type="radio" id="ans3" name="quest" value=${store.questions[question].answer[2]}>
-        <label for="other">${store.questions[question].answer[2]}</label><br>
-
-        <input type="radio" id="ans4" name="quest" value=${store.questions[question].answer[3]}>
-        <label for="other">${store.questions[question].answer[3]}</label>
-        </form> 
-        </div>
+ function loadQuestion(store) {
+    var currentQuestion = store.questions[store.questionNumber];
+    var templateHTML = `<div class="questions">
+    <h2 id="question">${currentQuestion.question}</h2>
+    <form>
+    <input type="radio" id="ans1" name="quest" value=${currentQuestion.answer[0]}>
+    <label for="male">${currentQuestion.answer[0]}</label><br>
+    <input type="radio" id="ans2" name="quest" value=${currentQuestion.answer[1]}>
+    <label for="female">${currentQuestion.answer[1]}</label><br>
+    <input type="radio" id="ans3" name="quest" value=${currentQuestion.answer[2]}>
+    <label for="other">${currentQuestion.answer[2]}</label><br>
+    <input type="radio" id="ans4" name="quest" value=${currentQuestion.answer[3]}>
+    <label for="other">${currentQuestion.answer[3]}</label>
+    </form>  
+    </div>
 
       <div>
         <button id="submit">Submit Answer</button>
       </div>`;
+
+    $('main').html(templateHTML);
+    $('#submit').on('click', () => {
+        loadAnswer(store);
+    });
 };
 
 
-function rightAns(store) {
-    let correct = store.questions[question].correctAnswer;
-    let wrong = 0;
-     console.log(correct, $('input[name="quest"]:checked').val());
-    if ($('input[name="quest"]:checked').val() === store.questions[question].correctAnswer) {
-    store.score++;
-    return `<div class="questions">
-    <h1 id="question">CORRECT!</h2>
+// // create function using 'store' as a parameter
+// // use variable 'createQuestion' to load each question 'store.questions[store.questionNumber]'
 
-    <button id="next">Next Question</button>
-      </div>`;
-      
+
+function loadAnswer(store) {
+    let correctAnswer = store.questions[store.questionNumber].correctAnswer;
+    let guess = $('input[type="radio"]:checked').val();
+    let templateHTML = '';
+    console.log(guess, correctAnswer);
+    if (guess === correctAnswer) {
+        store.score++;
+        templateHTML = `<div class="questions">
+   <h1 id="question">CORRECT!</h2>
+   <button id="next">Next Question</button>
+     </div>`;
     } else {
-        return `<div class="questions">
-    <h1 id="question">OH NO...</h2>
-
-    <h3>The correct answer is <br> ${store.questions[question].correctAnswer}<h3>
-
-    <button id="next">Next Question</button>
-      </div>`;
-      store.wrong++;
+        store.wrong++;
+        templateHTML = `<div class="questions">
+   <h1 id="question">OH NO...</h2>
+   <h3>The correct answer is <br> ${correctAnswer}<h3>
+   <button id="next">Next Question</button>
+     </div>`;
     }
-};
+    $('main').html(templateHTML);
+    $('#next').on('click', function () {
+        store.questionNumber++;
+        loadQuestion(store);
+    });
+}
 
+
+// new function for when the last question is asked, show the results of the quiz
 
 function results() {
     return `<div class="questions">
     <h1 id="question">And the Results Are...</h2>
 
-    <button id="next">Next Question</button>
+    <p class="fontSize"> You scored <br> ${store.score} / 4 <br>
       </div>`;
-}
+};
+
+
+
+
+
+
 
 
 
